98203d8dd474bb44a2dddaa36c8870ef42e55cdd
b0bW <robertwalks@gmail.com>
2017-09-12 00:27:34 -0400

    1x1 and 3x3 tile board demos

--- chessBoard.js
+++ chessBoard.js
@@ -6,13 +6,33 @@ let build = function (){
     let temprow=[]
     for (let cols=0; cols<8; cols++){
       ((rows.toString(2)^cols.toString(2))%10)?
-        temprow.push('#') : temprow.push('O')
+        temprow.push('#')
+        : temprow.push(' ')
     }
     board.push(temprow)
   
   }
   return board.map(r=>r.join(''))
-
 }
 console.log(  build())
 
+//build a mockup with 3x3 tiles 
+let build3by3 = function (){
+
+  let board = []  
+  for (let rows=0; rows<8; rows++){
+    let temprow=[]
+    for (let cols=0; cols<8; cols++){
+      ((rows.toString(2)^cols.toString(2))%10)?
+        temprow.push('###')
+        : temprow.push('   ')
+    }
+    for (let i=0; i<3; i++){
+      board.push(temprow)
+    }
+  
+  }
+  return board.map(r=>r.join(''))
+
+}
+console.log( build3by3())
